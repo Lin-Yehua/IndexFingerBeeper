e958bbd0855a638119ef4942d80109cf37836c8d
Lin-Yehua <zypp2605205@outlook.com>
2026-03-20 02:07:06 +0800

    适配ESP32S3

--- src/Key_Drv.cpp
+++ src/Key_Drv.cpp
@@ -1,77 +1,23 @@
 #include <Key_Drv.h>
 
-#define UP_PIN 15
-#define DOWN_PIN 16
+
 #define LEFT_PIN 2
-#define RIGHT_PIN 17
 
 uint8_t Keycode = 255;
 
 void Key_init()
 {
-    pinMode(UP_PIN,INPUT);
-    pinMode(DOWN_PIN,INPUT);
     pinMode(LEFT_PIN,INPUT);
-    pinMode(RIGHT_PIN,INPUT);
     Keycode = 255;
 }
 uint8_t get_Keystate()
 {
     uint8_t code = 255;
-    if (digitalRead(UP_PIN) == LOW)
-    {
-        code = 0;
-    }
-    else if (digitalRead(DOWN_PIN) == LOW)
-    {
-        code = 1;
-    }
-    else if (digitalRead(LEFT_PIN) == LOW)
+     if (digitalRead(LEFT_PIN) == LOW)
     {
         code = 2;
     }
-    else if (digitalRead(RIGHT_PIN) == LOW)
-    {
-        code = 3;
-    }
     
-    return code;
-}
-uint8_t get_Keystate_B()
-{
-    uint8_t code = 255;
-    if (digitalRead(UP_PIN) == LOW)
-    {
-        code |= 0x01;
-    }
-    else
-    {
-        code &= 0xFE;
-    }
-     if (digitalRead(DOWN_PIN) == LOW)
-    {
-        code |= 0x02;
-    }
-    else
-    {
-        code &= 0xFD;
-    }
-     if (digitalRead(LEFT_PIN) == LOW)
-    {
-        code |= 0x04;
-    }
-    else
-    {
-        code &= 0xFB;
-    }
-     if (digitalRead(RIGHT_PIN) == LOW)
-    {
-        code |= 0x08;
-    }
-    else
-    {
-        code &= 0xF7;
-    }
     return code;
 }
 void Key_loop()
@@ -92,26 +38,4 @@ uint8_t get_Keycode()
     Temp = Keycode;
     Keycode = 255;
     return Temp;
-}
-
-/*
-单次检测,keycode为4时为任意按键，适用于单次触发
-*/
-bool checkKey(uint8_t keycode)
-{
-    if (keycode != 4)
-    {
-      if(get_Keystate() == keycode)
-      {
-        return true;
-      }
-    }
-    else
-    {
-      if(get_Keystate() != 255)
-      {
-        return true;
-      }
-    }
-    return false;
 }
